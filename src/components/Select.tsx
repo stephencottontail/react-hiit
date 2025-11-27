61b5751179eeafae85442039d6136c5c981cbc7d
Stephen Dickinson <stephencottontail@me.com>
2025-11-27 01:44:23 -0700

    Add extra props for Select

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,10 +1,12 @@
 type SelectProps = React.SelectHTMLAttributes<HTMLSelectElement> & {
   className: string;
-  onChange: React.ChangeEvent<HTMLSelectElement>;
+  onChange: React.ChangeEventHandler<HTMLSelectElement>;
+  options: Array<React.OptionHTMLAttributes<HTMLOptionElement>>;
+  value: string;
 };
 
 const Option = (props: React.OptionHTMLAttributes<HTMLOptionElement>) => {
-  const { value, label, selected } = props;
+  const { value, label } = props;
 
   return (
     <option
@@ -16,12 +18,13 @@ const Option = (props: React.OptionHTMLAttributes<HTMLOptionElement>) => {
 };
 
 export const Select = (props: SelectProps) => {
-  const { className, onChange, options, ...rest } = props;
+  const { className, onChange, options, value, ...rest } = props;
 
   return (
     <select
       className={className}
       onChange={onChange}
+      value={value}
       {...rest}
     >
       {options && options.map((el, i) => (
