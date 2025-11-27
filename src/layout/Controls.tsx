280d3616892c52eea9f3bfe0a50fd7cd6fb13627
Stephen Dickinson <stephencottontail@me.com>
2025-11-27 06:42:57 -0700

    Set up option dropdowns

--- src/layout/Controls.tsx
+++ src/layout/Controls.tsx
@@ -7,5 +7,58 @@ type ControlsProps = ReturnType<typeof createControlsState> & {
 
 export const Controls = (props: ControlsProps) => {
   const { className, cycles, setCycles, work, setWork, ratio, setRatio } = props;
+
+  return (
+    <div
+      className={className}
+    >
+      <label>
+        <span>Cycles</span>
+        <Select
+          className={`${className}__select`}
+          value={cycles}
+          onChange={ (event: React.ChangeEvent<HTMLSelectElement>) => {
+            console.log(event.target.value);
+            setCycles(event.target.value);
+          }}
+          options={[
+            { label: '3', value: '3' },
+            { label: '4', value: '4' },
+            { label: '5', value: '5' },
+          ]}
+        />
+      </label>
+      <label>
+        <span>Work Time (in seconds)</span>
+        <Select
+          className={`${className}__select`}
+          value={work}
+          onChange={ (event: React.ChangeEvent<HTMLSelectElement>) => {
+            setWork(event.target.value);
+          }}
+          options={[
+            { label: '10', value: '10' },
+            { label: '15', value: '15' },
+            { label: '20', value: '20' },
+            { label: '25', value: '25' },
+          ]}
+        />
+      </label>
+      <label>
+        <span>Work-to-Recovery Ratio</span>
+        <Select
+          className={`${className}__select`}
+          value={ratio}
+          onChange={ (event: React.ChangeEvent<HTMLSelectElement>) => {
+            setRatio(event.target.value);
+          }}
+          options={[
+            { label: '3', value: '3' },
+            { label: '2', value: '2' },
+            { label: '1', value: '1' },
+          ]}
+        />
+      </label>
+    </div>
   );
 };
