1e019addb14fa708bcfdfb302c4ec1dbba573278
Stephen Dickinson <stephencottontail@me.com>
2025-12-02 11:45:20 -0700

    Rename ref because we now use `setTimeout`

--- src/utils/createTimerState.ts
+++ src/utils/createTimerState.ts
@@ -3,14 +3,14 @@ import { useState, useRef } from 'react';
 export const createTimerState = () => {
   const [elapsed, setElapsed] = useState(0);
   const [referenceTime, setReferenceTime] = useState(Date.now());
-  const interval = useRef<number | undefined>(undefined);
+  const timeout = useRef<ReturnType<typeof setTimeout>>(undefined);
 
   return {
     elapsed,
     setElapsed,
     referenceTime,
     setReferenceTime,
-    interval,
+    timeout,
   };
 }
 
