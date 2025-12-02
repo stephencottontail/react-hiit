bc630fdd4249de0c6232c0e8e14f38c2ea66e4c4
Stephen Dickinson <stephencottontail@me.com>
2025-12-02 07:29:29 -0700

    Track a reference time
    We need this for a more accurate timer

--- src/utils/createTimerState.ts
+++ src/utils/createTimerState.ts
@@ -2,11 +2,14 @@ import { useState, useRef } from 'react';
 
 export const createTimerState = () => {
   const [elapsed, setElapsed] = useState(0);
+  const [referenceTime, setReferenceTime] = useState(Date.now());
   const interval = useRef<number | undefined>(undefined);
 
   return {
     elapsed,
     setElapsed,
+    referenceTime,
+    setReferenceTime,
     interval,
   };
 }
